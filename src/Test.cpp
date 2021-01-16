362fb0ac760aa98abbfb97b8c2bbdee95b31fb35
Mücahid IŞIK <56644702+MucahidISIK@users.noreply.github.com>
2021-01-16 19:30:53 +0300

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,11 +1,4 @@
-﻿/**
-* @file b181210111_1A_Odev2
-* @description Disaridan bir txt dosyasindan okutulan bilgilerin doubly linked liste yerlestirilip daha sonra ekrana bastirilmasi
-* @course 1. Öğretim A Grubu
-* @assignment 2. Ödev
-* @date 26.11.2019
-* @author Mücahid IŞIK mucahid.isik@ogr.sakarya.edu.tr , Hüseyin HIRA huseyin.hira@ogr.sakarya.edu.tr 
-*/
+
 #include "Node.h"
 #include "LinkedList.h"
 #include "ListIterator.h"
